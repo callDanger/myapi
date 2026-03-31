f346fede15aec78a2fb1f79ab30317b230a01d6c
Tony Liu <spock2085@gmail.com>
2026-03-31 15:57:35 +0800

    添加sqllite

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from pydantic import BaseModel
+import models
+from database import engine, get_db
 
-# 创建 FastAPI 实例
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+
+
+class UserCreate(BaseModel):
+    name: str
+
 
 # 测试接口
 @app.get("/")
 def home():
     return {"message": "FastAPI 服务运行成功！"}
 
+
 # 测试带参数的接口
 @app.get("/user/{name}")
 def get_user(name: str):
     return {"欢迎你": name}
+
+
+# 新增用户接口
+@app.post("/set_user")
+def set_user(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = models.User(name=user.name)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return {"id": db_user.id, "name": db_user.name}
---
+++ models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
